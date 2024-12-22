f16d6d12408d6006821ab5f372a9fd528ac0f3b8
Akash Singh <akash_s@me.iitr.ac.in>
2024-12-22 17:38:59 +0530

    Modified GUI

--- helper-functions/frontend.js
+++ helper-functions/frontend.js
@@ -15,20 +15,22 @@ function getHTML(webview){
         <link rel="stylesheet" href="${styleUri}">
         </head>
         <body>
-            <p id="p1">Paste problem URL to get test cases</p>
-            <form id="fetchForm">
-                <input id='urlInput' type="text" placeholder="https://www.leetcode.com/problems/problem_name">
-                <br>
-                <button type="submit" id="getButton">Fetch test cases</button>
-            </form>
-            <p id="fetchResult"></p>
-            <br><br>
-            <div class="run">
-                <p>If you have fetched sample test cases then you can run your code on them locally</p>
-                <p>Click below button to automatically execute your code against them</p>
-                <form id="runForm">
-                    <input id='nameInput' type="text" placeholder="Problem Name">
+            <div class="container">
+                <p id="p1">Paste problem URL to get test cases</p>
+                <form class="fomo" id="fetchForm">
+                    <input id='urlInput' type="text" placeholder="https://www.leetcode.com/problems/problem_name">
+                    <button type="submit" id="getButton">Fetch test cases</button>
+                </form>
+                <p id="fetchResult"></p>
+            </div>
+            <div class="container">
+                <div class="runningText">
+                    If you have fetched sample test cases then you can run your code on them locally.
                     <br>
+                    Click below button to automatically execute your code against them.
+                </div>
+                <form class="fomo" id="runForm">
+                    <input id='nameInput' type="text" placeholder="Problem Name">
                     <button type="submit" id="runCode">Test</button>
                 </form>
                 <p id="runResult"></p>
